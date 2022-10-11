7ae494556d444a1f9e4713a415f6fa921d0e01ff
Enrique Shunnar <xtealer00@gmail.com>
2022-10-11 04:37:38 -0500

    Update title styles

--- components/headers/PrimaryHeader.tsx
+++ components/headers/PrimaryHeader.tsx
@@ -2,10 +2,21 @@ import { FC } from "react";
 
 export interface PrimaryHeaderProps {
   title?: string;
+  center?: boolean;
 }
 
-const PrimaryHeader: FC<PrimaryHeaderProps> = ({ title }) => {
-  return <h1 className="mb-4 text-app-green text-3xl font-bold">{title}</h1>;
+const PrimaryHeader: FC<PrimaryHeaderProps> = ({ title, center = true }) => {
+  return (
+    <h1
+      className={
+        center
+          ? "mb-4 text-app-green text-3xl font-bold text-center mx-auto"
+          : "mb-4 text-app-green text-3xl font-bold"
+      }
+    >
+      {title}
+    </h1>
+  );
 };
 
 export default PrimaryHeader;
--- components/headers/SecondaryHeader.tsx
+++ components/headers/SecondaryHeader.tsx
@@ -2,10 +2,24 @@ import { FC } from "react";
 
 export interface SecondaryHeaderProps {
   title?: string;
+  center?: boolean;
 }
 
-const SecondaryHeader: FC<SecondaryHeaderProps> = ({ title }) => {
-  return <h1 className="mb-4 text-black text-3xl font-bold">{title}</h1>;
+const SecondaryHeader: FC<SecondaryHeaderProps> = ({
+  title,
+  center = true,
+}) => {
+  return (
+    <h1
+      className={
+        center
+          ? "mb-4 text-black text-3xl font-bold text-center mx-auto"
+          : "mb-4 text-black text-3xl font-bold"
+      }
+    >
+      {title}
+    </h1>
+  );
 };
 
 export default SecondaryHeader;
